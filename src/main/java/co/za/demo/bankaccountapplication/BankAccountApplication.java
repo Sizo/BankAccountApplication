260748b5de6ca6597d694ceae83f536ab42b0b47
Sizo Duma <Sizo.duma@sanlam.co.za>
2025-09-12 13:16:09 +0200

    initialize project

---
+++ src/main/java/co/za/demo/bankaccountapplication/BankAccountApplication.java
@@ -0,0 +1,13 @@
+package co.za.demo.bankaccountapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/za/demo/bankaccountapplication/BankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.demo.bankaccountapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
